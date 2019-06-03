f57da77bde6e579dc8cedbc554eb509113ec8c47
jamieberrier <berrier.jamie@gmail.com>
2019-06-03 19:55:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,7 @@ class Student
       self.update
     else
      sql = <<-SQL
-       INSERT INTO songs (name, album)
+       INSERT INTO students (name, grade)
        VALUES (?, ?)
      SQL
      DB[:conn].execute(sql, self.name, self.album)
