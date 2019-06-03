1ceb47dfbe470ad0a75ba26fd69d3b3b7436f2e5
jamieberrier <berrier.jamie@gmail.com>
2019-06-03 20:28:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,11 +47,11 @@ class Student
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
-   end
+  end
 
-   def self.create(name:, grade:)
-    student = Student.new(name, grade)
-    student.save
-    student
+ def self.create(name:, grade:)
+  student = Student.new(name, grade)
+  student.save
+  student
   end
 end
